a6f76967d37cc0010508bbe75b0341b358d059e7
racem-cherni <racem.cherni@esprit.tn>
2020-05-30 11:41:03 +0100

    u

--- KinderGartenProject/src/main/java/tn/esprit/spring/entities/UserApp.java
+++ KinderGartenProject/src/main/java/tn/esprit/spring/entities/UserApp.java
@@ -1,8 +1,9 @@
 package tn.esprit.spring.entities;
 
-import java.io.Serializable;
+import java.io.Serializable;  
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
@@ -54,6 +55,13 @@ public class UserApp implements Serializable {
 @OneToMany(cascade = CascadeType.ALL, mappedBy="sourceUser",fetch=FetchType.LAZY)
 private Collection<Advertissement> advertissemented= new ArrayList<>();
 
+
+
+
+
+
+
+
 public UserApp(String username, String password, Collection<RoleApp> roles, boolean actived, int score,int point) {
 	super();
 	
@@ -77,6 +85,67 @@ public UserApp( String username, String password, Collection<RoleApp> roles, boo
 public UserApp() {
 	super();
 }
+public long getId() {
+	return id;
+}
+public void setId(long id) {
+	this.id = id;
+}
+public String getUsername() {
+	return username;
+}
+public void setUsername(String username) {
+	this.username = username;
+}
+public String getPassword() {
+	return password;
+}
+public void setPassword(String password) {
+	this.password = password;
+}
+public Collection<RoleApp> getRoles() {
+	return roles;
+}
+public void setRoles(Collection<RoleApp> roles) {
+	this.roles = roles;
+}
+public boolean isActived() {
+	return actived;
+}
+public void setActived(boolean actived) {
+	this.actived = actived;
+}
+public int getScore() {
+	return score;
+}
+public void setScore(int score) {
+	this.score = score;
+}
+public int getPoint() {
+	return point;
+}
+public void setPoint(int point) {
+	this.point = point;
+}
+public Parent getParent() {
+	return parent;
+}
+public void setParent(Parent parent) {
+	this.parent = parent;
+}
+public KinderGarten getKindergarten() {
+	return kindergarten;
+}
+public void setKindergarten(KinderGarten kindergarten) {
+	this.kindergarten = kindergarten;
+}
+public Collection<Advertissement> getAdvertissemented() {
+	return advertissemented;
+}
+public void setAdvertissemented(Collection<Advertissement> advertissemented) {
+	this.advertissemented = advertissemented;
+}
+
 
 
 
