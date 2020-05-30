8cc950e395890b717c577f00b917960a8e921f70
racem-cherni <racem.cherni@esprit.tn>
2020-05-30 11:30:13 +0100

    com

--- KinderGartenProject/src/main/java/tn/esprit/spring/Service/AccountService.java
+++ KinderGartenProject/src/main/java/tn/esprit/spring/Service/AccountService.java
@@ -1,6 +1,6 @@
 package tn.esprit.spring.Service;
 
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Service; 
 
 import tn.esprit.spring.entities.RoleApp;
 import tn.esprit.spring.entities.UserApp;
@@ -12,4 +12,7 @@ public UserApp saveUser(UserApp user);
 public RoleApp saveRole(RoleApp role);
 public UserApp findUserByUsername(String username);
 public UserApp addRoleToUser(String username,String role);
+
+ 
+
 }
