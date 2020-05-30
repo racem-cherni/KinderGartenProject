31b60375adf5f717517911dc06c5175cce07e944
racem-cherni <racem.cherni@esprit.tn>
2020-05-30 16:25:12 +0100

    update parent event relation

--- KinderGartenProject/src/main/java/tn/esprit/spring/entities/Parent.java
+++ KinderGartenProject/src/main/java/tn/esprit/spring/entities/Parent.java
@@ -1,6 +1,6 @@
 package tn.esprit.spring.entities;
 
-import java.io.Serializable;
+import java.io.Serializable; 
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Date;
@@ -15,14 +15,14 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 @Entity
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
+
 public class Parent implements Serializable{
 	@Id @GeneratedValue(strategy=GenerationType.AUTO)
 	private Long id;
@@ -32,11 +32,89 @@ public class Parent implements Serializable{
 	private String Email;
 	private String adresse;
 	private int tel;
+	@Temporal(TemporalType.DATE)
 	private Date dateNaissance;
 	@OneToOne
 	private UserApp userApp;
 	@OneToMany(cascade = CascadeType.ALL, mappedBy="parents",fetch=FetchType.LAZY)
 	private Collection<Child> childs= new ArrayList<>();
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getImage() {
+		return image;
+	}
+	public void setImage(String image) {
+		this.image = image;
+	}
+	public String getEmail() {
+		return Email;
+	}
+	public void setEmail(String email) {
+		Email = email;
+	}
+	public String getAdresse() {
+		return adresse;
+	}
+	public void setAdresse(String adresse) {
+		this.adresse = adresse;
+	}
+	public int getTel() {
+		return tel;
+	}
+	public void setTel(int tel) {
+		this.tel = tel;
+	}
+	public Date getDateNaissance() {
+		return dateNaissance;
+	}
+	public void setDateNaissance(Date dateNaissance) {
+		this.dateNaissance = dateNaissance;
+	}
+	public UserApp getUserApp() {
+		return userApp;
+	}
+	public void setUserApp(UserApp userApp) {
+		this.userApp = userApp;
+	}
+	public Collection<Child> getChilds() {
+		return childs;
+	}
+	public void setChilds(Collection<Child> childs) {
+		this.childs = childs;
+	}
+	public Parent(Long id, String firstName, String lastName, String image, String email, String adresse, int tel,
+			Date dateNaissance, UserApp userApp, Collection<Child> childs) {
+		super();
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.image = image;
+		Email = email;
+		this.adresse = adresse;
+		this.tel = tel;
+		this.dateNaissance = dateNaissance;
+		this.userApp = userApp;
+		this.childs = childs;
+	}
+	public Parent() {
+		super();
+	}
 
 	
 	/////////////////////////////////////////////////////////////////////////
@@ -67,21 +145,10 @@ public class Parent implements Serializable{
 		this.evaluations = evaluations;
 	}
 
-	public Collection<Child> getChilds() {
-		return childs;
-	}
+	
 
-	public void setChilds(Collection<Child> childs) {
-		this.childs = childs;
-	}
 
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
+	
 
 	public List<Discussion_Event> getDiscussions() {
 		return discussions;
@@ -91,21 +158,6 @@ public class Parent implements Serializable{
 		this.discussions = discussions;
 	}
 
-	public String getFirstName() {
-		return firstName;
-	}
-
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-
-	public String getLastName() {
-		return lastName;
-	}
-
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
 	
 
 	
