c3fd8163fd29f1ef43eabf041f033a5181f33112
racem-cherni <racem.cherni@esprit.tn>
2020-05-30 11:40:38 +0100

    r

--- KinderGartenProject/src/main/java/tn/esprit/spring/entities/RoleApp.java
+++ KinderGartenProject/src/main/java/tn/esprit/spring/entities/RoleApp.java
@@ -22,4 +22,17 @@ public class RoleApp implements Serializable  {
 private Long id;
 @Column(unique=true)
 private String roleName;
+public Long getId() {
+	return id;
+}
+public void setId(Long id) {
+	this.id = id;
+}
+public String getRoleName() {
+	return roleName;
+}
+public void setRoleName(String roleName) {
+	this.roleName = roleName;
+}
+
 }
