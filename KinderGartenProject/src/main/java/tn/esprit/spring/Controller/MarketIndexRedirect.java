248b9b638f9b84d60dc57cf0f4ee717b00fcbb56
majdilahsini <majdi.lahsini@esprit.tn>
2020-06-01 20:41:58 +0200

    update name

--- KinderGartenProject/src/main/java/tn/esprit/spring/Controller/MarketIndexRedirect.java
+++ KinderGartenProject/src/main/java/tn/esprit/spring/Controller/MarketIndexRedirect.java
@@ -3,6 +3,6 @@ package tn.esprit.spring.Controller;
 import org.ocpsoft.rewrite.annotation.Join;
 
 @Join(path = "/index", to = "/pages/parent/marketplace/index.jsf")
-public class testingthis {
+public class MarketIndexRedirect {
 
 }
