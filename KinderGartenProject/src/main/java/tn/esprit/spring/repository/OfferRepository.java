17cb22948e2c138f9ecc5d5916796cc40d4ffb85
majdilahsini <majdi.lahsini@esprit.tn>
2020-06-05 17:53:48 +0200

    fix user / user app bug

--- KinderGartenProject/src/main/java/tn/esprit/spring/repository/OfferRepository.java
+++ KinderGartenProject/src/main/java/tn/esprit/spring/repository/OfferRepository.java
@@ -14,17 +14,17 @@ import tn.esprit.spring.entities.Offer;
 public interface OfferRepository extends CrudRepository <Offer,Integer>{
 	
 	@Query("SELECT count(*) FROM Offer o"
-		 + " WHERE o.Kindergarten.user.id =:userid AND o.product.id=:productid")
+		 + " WHERE o.Kindergarten.userapp.id =:userid AND o.product.id=:productid")
     public int CheckProduct(@Param("userid") long userid, @Param("productid")int productid);
 	
 	@Query("SELECT count(*) FROM Offer o"
-			 + " WHERE o.Kindergarten.user.id =:userid")
+			 + " WHERE o.Kindergarten.userapp.id =:userid")
 	public int getOfferCount(@Param("userid") long userid);
 	
 	@Query("SELECT o FROM Offer o WHERE o.Kindergarten.userapp.id =:userid AND o.product.id =:productid")
     public Offer getExistedOffer(@Param("userid") long userid, @Param("productid") int productid);
 	
-	@Query("SELECT o FROM Offer o WHERE o.Kindergarten.user.id =:id GROUP BY o.product")
+	@Query("SELECT o FROM Offer o WHERE o.Kindergarten.userapp.id =:id GROUP BY o.product")
 	public List<Offer> getOffersByKindergarten(@Param ("id") Long id);
 	
 	@Query("SELECT o FROM Offer o WHERE o.product.id = :id GROUP BY o.id")
